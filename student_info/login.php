797b245c84427d37d312935e24d9d1e548a315f0
begadilseka@gmail.com <begadilseka@gmail.com@0e56b27a-6ce6-84fc-4e94-4b4d0fc0a7c8>
2015-02-19 18:52:53 +0000

    login page

---
+++ student_info/login.php
@@ -0,0 +1,30 @@
+<?php
+
+	
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/mystyle.css" />
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700,400italic' rel='stylesheet' type='text/css'>
+</head>
+<body class="login_body">
+    
+    <div id="login_form">
+        <div id="colors_bar"></div>
+        
+        <div class="inputs">
+            <input type="text" placeholder="Login" id="login" />
+            <input type="password" placeholder="Password" id="password" />
+            <button onclick="js">SIGN IN</button>
+        </div>
+        
+    </div>
+
+</body>
+</html>
