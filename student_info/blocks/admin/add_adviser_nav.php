c31992ade573c08ba51e9817b3a574a0a90f7890
begadilseka@gmail.com <begadilseka@gmail.com@0e56b27a-6ce6-84fc-4e94-4b4d0fc0a7c8>
2015-04-26 22:39:15 +0000

    . . .

---
+++ student_info/blocks/admin/add_adviser_nav.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.php">STUDENT INFO</a>
+		</div>
+		<div class="collapse navbar-collapse navbar-right">
+			<ul class="nav navbar-nav">
+				<li>
+					<a href="admin.php">admin</a>
+				</li>
+			
+				<li>
+					<a href="#">add student</a>
+				</li>
+				
+				<li>
+					<a href="blocks/logout.php">logout</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
