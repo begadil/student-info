5c573e09c27c5281c17bf2d79f79c2654110ab2f
begadilseka <begadilseka@gmail.com>
2015-04-26 20:16:24 +0000

    . . .

--- student_info/blocks/right.php
+++ student_info/blocks/right.php
@@ -11,6 +11,7 @@
 			        		</a>
 			      		</h4>
 			    	</div>
+			    	
 			    	<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
 			      		<div class="panel-body">
 			      			<div class="row">
@@ -21,6 +22,13 @@
 									</div>
 									<div class="form-group">
 										<input type="text" class="form-control" placeholder="enter surname" id="search_surname" onKeyUp="show_result()"/>
+									</div>
+									<div class="form-group">
+										<label class="control-label">Gender: </label>
+										<div class="col-lg-12">
+											<label class = "radio-inline"><input type="radio" name="search_gender" value = 'male' onClick="show_result()">male</label>
+											<label class = "radio-inline"><input type="radio" name="search_gender" value = 'female' onClick="show_result()">female</label>
+										</div>
 									</div>
 								</form>
 			      				<div class="col-lg-2"></div>
@@ -33,7 +41,7 @@
 			    	<div class="panel-heading" role="tab" id="headingTwo">
 			      		<h4 class="panel-title">
 			        		<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-			          			sdu info
+			          			address
 			        		</a>
 			      		</h4>
 			    	</div>
@@ -42,7 +50,34 @@
 			      			<div class="col-lg-1"></div>
 			      			<form class="col-lg-9 form-horizontal" role="form">
 								<div class="form-group">
-									<input type="text" class="form-control" placeholder="enter sdu_id" id="search_sdu_id" onKeyUp="show_result()"/>
+									<label class="control-label">Address type: </label>
+									<div class="col-lg-12">
+										<label class="radio-inline"><input type="radio" name="search_address_type" value = 'home' onClick="show_result()" checked="checked">home</label>
+										<label class="radio-inline"><input type="radio" name="search_address_type" value = 'current' onClick="show_result()">current</label>
+									</div>
+								</div>
+								<div class="form-group">
+									<select id = "republic" class="form-control" onchange="republic_selected()">
+										<option value = "0">republic</option>
+										<?php 
+											$q = mysql_query("select id, name from republic");
+											while($a = mysql_fetch_array($q)){
+												echo "<option value = '$a[id]'>$a[name]</option>";
+											}
+										?>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<select id = "region" class="form-control" onchange="region_selected()">
+										<option value = "0">region</option>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<select id = "city" class="form-control" onchange="show_result()">
+										<option value = "0">city</option>
+									</select>
 								</div>
 							</form>
 			      			<div class="col-lg-2"></div>
@@ -54,15 +89,103 @@
 			    	<div class="panel-heading" role="tab" id="headingThree">
 			      		<h4 class="panel-title">
 			        		<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-			          			other info
+			          			sdu info
 			        		</a>
 			      		</h4>
 			    	</div>
 			    	<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+			      		<div class="row">
+			      			<div class="col-lg-1"></div>
+			      			<form class="col-lg-9 form-horizontal" role="form">
+			      			
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="enter sdu_id" id="search_sdu_id" onKeyUp="show_result()"/>
+								</div>
+								
+								<div class="form-group">
+									<select id = "faculty" class="form-control" onchange="faculty_selected()">
+										<option value = "0">faculty</option>
+										<?php 
+											$q = mysql_query("select id, name from faculty");
+											while($a = mysql_fetch_array($q)){
+												echo "<option value = '$a[id]'>$a[name]</option>";
+											}
+										?>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<select id = "department" class="form-control" onchange="department_selected()">
+										<option value = "0">department</option>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<select id = "course" class="form-control" onchange="course_selected()">
+										<option value = "0">course</option>
+										<option value = "1">I</option>
+										<option value = "2">II</option>
+										<option value = "3">III</option>
+										<option value = "4">IV</option>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<select id = "group" class="form-control" onchange="show_result()">
+										<option value = "0">group</option>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<select id = "grant_type" class="form-control" onchange="show_result()">
+										<option value = "">grant_type</option>
+										<option value = "SDU grant">SDU grant</option>
+										<option value = "State grant">State grant</option>
+										<option value = "Paid">Paid</option>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<label class="control-label">Stipend: </label>
+									<div class="col-lg-12">
+						  				<label class="radio-inline"><input type="radio" name="search_stipend" value = 'yes' onClick="show_result()">yes</label>
+										<label class="radio-inline"><input type="radio" name="search_stipend" value = 'no' onClick="show_result()">no</label>
+									</div>
+								</div>
+								
+								<div class="form-group">
+									<label class="control-label">GPA: </label>
+									<div class="form-group row">
+										<div class="col-lg-6">
+											<input type="text" class="form-control" placeholder="min gpa" id="min_gpa" onKeyUp="show_result()"/>
+										</div>
+									 	<div class="col-lg-6">
+									 		<input type="text" class="form-control" placeholder="max gpa" id="max_gpa" onKeyUp="show_result()"/>
+									 	</div>
+										
+									</div>
+								</div>
+								
+							</form>
+			      			<div class="col-lg-2"></div>
+			      		</div>
+			    	</div>
+			  	</div>
+			  	
+			  	<div class="panel panel-info">
+			    	<div class="panel-heading" role="tab" id="headingFour">
+			      		<h4 class="panel-title">
+			        		<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+			          			other info
+			        		</a>
+			      		</h4>
+			    	</div>
+			    	<div id="#collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
 			      		<div class="panel-body">
 			      			some texts
 			      		</div>
 			    	</div>
 			  	</div>
+			  	
 			</div>
 		</div>
--- student_info/php/php_functions.php
+++ student_info/php/php_functions.php
@@ -24,25 +24,270 @@
 		/************************************************************************/
 		
 		/******************************* INDEX PAGE *******************************/
+		
+		elseif($function == "get_department"){
+				
+			$faculty_id = $_REQUEST['faculty_id'];
+			$q = mysql_query("select * from department where faculty_id='$faculty_id'");
+			echo "<option value = '0'>department</option>";
+			while($a = mysql_fetch_array($q)){
+				echo "<option value = '$a[id]'>$a[name]</option>";
+			}
+				
+		}
+		
+		elseif($function == "get_group"){
+		
+			$department_id = $_REQUEST['department_id'];
+			$course = $_REQUEST['course'];
+			if($course == 0){
+				$q = mysql_query("select * from `group` where department_id = '$department_id'");
+			}
+			else{
+				$q = mysql_query("select * from `group` where department_id = '$department_id' and course = '$course'");
+			}
+			echo "<option value = '0'>group</option>";
+			while($a = mysql_fetch_array($q)){
+				echo "<option value = '$a[id]'>$a[name]</option>";
+			}
+		
+		}
+		
+		elseif($function == "get_region"){
+			
+			$republic_id = $_REQUEST['republic_id'];
+			$q = mysql_query("select * from region where republic_id='$republic_id'");
+			echo "<option value = '0'>region</option>";
+			while($a = mysql_fetch_array($q)){
+				echo "<option value = '$a[id]'>$a[name]</option>";
+			}
+			
+		}
+		
+		elseif($function == "get_city"){
+				
+			$region_id = $_REQUEST['region_id'];
+			$q = mysql_query("select * from city where region_id='$region_id'");
+			echo "<option value = '0'>city</option>";
+			while($a = mysql_fetch_array($q)){
+				echo "<option value = '$a[id]'>$a[name]</option>";
+			}
+				
+		}
+		
 		elseif ($function == "search"){
 			
 			$name = $_REQUEST['search_name'];
 			$surname = $_REQUEST['search_surname'];
+			$gender = $_REQUEST['search_gender'];
+			
+			$address_type = $_REQUEST['search_address_type'];
+			$republic = $_REQUEST['search_republic'];
+			$region = $_REQUEST['search_region'];
+			$city = $_REQUEST['search_city'];
+			
 			$sdu_id = $_REQUEST['search_sdu_id'];
+			$faculty = $_REQUEST['search_faculty'];
+			$department = $_REQUEST['search_department'];
+			$course = $_REQUEST['search_course'];
+			$group = $_REQUEST['search_group'];
 			
-			$query = "select * from student where ";
-			
-			if($name != ""){
-				$query .= " name_en like '%$name%' ";
-			}
-			if ($surname != ""){
-				if($query != "select * from student where "){
-					$query .= " and ";
+			if(($sdu_id == "" && $faculty == 0 && $department == 0 && $course == 0 && $group == 0) && ($republic == 0 && $region == 0 && $city == 0)){
+				$query = "select * from student where ";
+				
+				if($name != ""){
+					$query .= " name_en like '%$name%' ";
 				}
-				$query .= " surname_en like '%$surname%' ";
+				
+				if ($surname != ""){
+					if($query != "select * from student where "){
+						$query .= " and ";
+					}
+					$query .= " surname_en like '%$surname%' ";
+				}
+				
+				if ($gender != ""){
+					if($query != "select * from student where "){
+						$query .= " and ";
+					}
+					$query .= " gender = '$gender' ";
+				}
+			}
+			
+			elseif($name == "" && $surname == "" && $gender == "" && $republic == 0 && $region == 0 && $city == 0){
+				if($course == 0){
+					$query = "select st.* from student as st, sdu_info as si where st.sdu_info_id = si.id ";
+				}
+				else{
+					$query = "select st.* from student as st, sdu_info as si where st.sdu_info_id = si.id and course = '$course' ";
+				}
+				
+				if($sdu_id != ""){
+					$query .= " and si.sdu_id like '%$sdu_id%' ";
+				}
+				
+				if($faculty != 0){
+					if($query != "select st.* from student as st, sdu_info as si where st.sdu_info_id = si.id and " || $query != "select st.* from student as st, sdu_info as si where st.sdu_info_id = si.id and course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " si.faculty_id = '$faculty' ";
+				}
+				
+				if($department != 0){
+					if($query != "select st.* from student as st, sdu_info as si where st.sdu_info_id = si.id and " || $query != "select st.* from student as st, sdu_info as si where st.sdu_info_id = si.id and course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " si.department_id = '$department' ";
+				}
+				
+				if($group != 0){
+					if($query != "select st.* from student as st, sdu_info as si where st.sdu_info_id = si.id and " || $query != "select st.* from student as st, sdu_info as si where st.sdu_info_id = si.id and course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " si.group_id = '$group' ";
+				}
+			}
+			
+			elseif(($name == "" && $surname == "" && $gender == "") && ($sdu_id == "" && $faculty == 0 && $department == 0 && $course == 0 && $group == 0)){
+				if($address_type == "home"){
+					$query = "select st.* from student as st, address as ad where st.home_address_id = ad.id and ";
+				}
+				else{
+					$query = "select st.* from student as st, address as ad where st.current_address_id = ad.id and ";
+				}
+				
+				if($republic != 0){
+					$query .= " ad.republic_id = '$republic' ";
+				}
+				
+				if($region != 0){
+					if($query != "select st.* from student as st, address as ad where st.home_address_id = ad.id and " || $query != "select st.* from student as st, address as ad where st.current_address_id = ad.id and "){
+						$query .= " and ";
+					}
+					$query .= " ad.region_id = '$region' ";
+				}
+				
+				if($city != 0){
+					if($query != "select st.* from student as st, address as ad where st.home_address_id = ad.id and " || $query != "select st.* from student as st, address as ad where st.current_address_id = ad.id and "){
+						$query .= " and ";
+					}
+					$query .= " ad.city_id = '$city' ";
+				}
+			}
+			
+			elseif(($sdu_id == "" || $faculty != 0 || $department != 0 || $course != 0 || $group != 0) || ($name != "" || $surname != "" || $gender != "") || ($republic != 0 || $region != 0 || $city != 0)){
+				
+				if($address_type == "home"){
+					$query = "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id ";
+				}
+				else{
+					$query = "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id ";
+				}
+				
+				if($course != 0){
+					$query .= " and si.course = '$course' ";
+				}
+				
+				if($name != ""){
+					$query .= " and st.name_en like '%$name%' ";
+				}
+				
+				if ($surname != ""){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " st.surname_en like '%$surname%' ";
+				}
+				
+				if ($gender != ""){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " gender = '$gender' ";
+				}
+				
+				if($sdu_id != ""){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " si.sdu_id like '%$sdu_id%' ";
+				}
+				
+				if($faculty != 0){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " si.faculty_id = '$faculty' ";
+				}
+				
+				if($department != 0){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " si.department_id = '$department' ";
+				}
+				
+				if($group != 0){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " si.group_id = '$group' ";
+				}
+				
+				if($republic != 0){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " ad.republic_id = '$republic' ";
+				}
+				
+				if($region != 0){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " ad.region_id = '$region' ";
+				}
+				
+				if($city != 0){
+					if($query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id " || 
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.home_address_id = ad.id and si.course = '$course' " ||
+					   $query != "select st.* from student as st, address as ad, sdu_info as si where st.current_address_id = ad.id and si.course = '$course' "){
+						$query .= " and ";
+					}
+					$query .= " ad.city_id = '$city' ";
+				}
+				
 			}
 			$q = mysql_query($query);
+			echo $query;
 			$q_n = mysql_num_rows($q);
+			
+			
 			if($q_n > 0){
 				while($a = mysql_fetch_array($q)){
 					echo $a['name_en']." ".$a['surname_en'];
