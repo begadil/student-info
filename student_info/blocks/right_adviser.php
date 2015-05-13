33021f7507c6ff45f99fb252a2977672b86e68c6
begadilseka <begadilseka@gmail.com>
2015-05-13 03:56:11 +0000

    . . .

---
+++ student_info/blocks/right_adviser.php
@@ -0,0 +1,143 @@
+		<div id="sidebar-wrapper">
+			
+			<h3>Search</h3>
+			<p class='text-center' id="clear_a">
+				<a href='javascript:reset()'><i class="fa fa-times"> reset</i></a>
+			</p>
+			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+				<div class="panel panel-info">
+			    	<div class="panel-heading" role="tab" id="headingOne">
+			      		<h4 class="panel-title">
+			        		<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+			          			name or id 
+			        		</a>
+			      		</h4>
+			    	</div>
+			    	
+			    	<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+			      		<div class="panel-body">
+			      			<div class="row">
+			      				<div class="col-lg-1"></div>
+			      				<form class="col-lg-9 form-horizontal" role="form">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="enter name" id="search_name" onKeyUp="show_result()"/>
+									</div>
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="enter surname" id="search_surname" onKeyUp="show_result()"/>
+									</div>
+									<div class="form-group">
+										<label class="control-label">Gender: </label>
+										<div class="col-lg-12">
+											<label class = "radio-inline"><input type="radio" name="search_gender" value = 'male' onClick="show_result()">male</label>
+											<label class = "radio-inline"><input type="radio" name="search_gender" value = 'female' onClick="show_result()">female</label>
+										</div>
+									</div>
+								</form>
+			      				<div class="col-lg-2"></div>
+			      			</div>
+			      		</div>
+			    	</div>
+			  	</div>
+			  	
+			  	<div class="panel panel-info">
+			    	<div class="panel-heading" role="tab" id="headingTwo">
+			      		<h4 class="panel-title">
+			        		<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			          			address
+			        		</a>
+			      		</h4>
+			    	</div>
+			    	<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+			      		<div class="row">
+			      			<div class="col-lg-1"></div>
+			      			<form class="col-lg-9 form-horizontal" role="form">
+								<div class="form-group">
+									<label class="control-label">Address type: </label>
+									<div class="col-lg-12">
+										<label class="radio-inline"><input type="radio" name="search_address_type" value = 'home' onClick="show_result()" checked="checked">home</label>
+										<label class="radio-inline"><input type="radio" name="search_address_type" value = 'current' onClick="show_result()">current</label>
+									</div>
+								</div>
+								<div class="form-group">
+									<select id = "republic" class="form-control" onchange="republic_selected()">
+										<option value = "0">republic</option>
+										<?php 
+											$q = mysql_query("select id, name from republic");
+											while($a = mysql_fetch_array($q)){
+												echo "<option value = '$a[id]'>$a[name]</option>";
+											}
+										?>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<select id = "region" class="form-control" onchange="region_selected()">
+										<option value = "0">region</option>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<select id = "city" class="form-control" onchange="show_result()">
+										<option value = "0">city</option>
+									</select>
+								</div>
+							</form>
+			      			<div class="col-lg-2"></div>
+			      		</div>
+			    	</div>
+			  	</div>
+			  	
+			  	<div class="panel panel-info">
+			    	<div class="panel-heading" role="tab" id="headingThree">
+			      		<h4 class="panel-title">
+			        		<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+			          			sdu info
+			        		</a>
+			      		</h4>
+			    	</div>
+			    	<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+			      		<div class="row">
+			      			<div class="col-lg-1"></div>
+			      			<form class="col-lg-9 form-horizontal" role="form">
+			      			
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="enter sdu_id" id="search_sdu_id" onKeyUp="show_result()"/>
+								</div>
+								
+								<div class="form-group">
+									<select id = "grant_type" class="form-control" onchange="show_result()">
+										<option value = "">grant_type</option>
+										<option value = "SDU grant">SDU grant</option>
+										<option value = "State grant">State grant</option>
+										<option value = "Paid">Paid</option>
+									</select>
+								</div>
+								
+								<div class="form-group">
+									<label class="control-label">Stipend: </label>
+									<div class="col-lg-12">
+						  				<label class="radio-inline"><input type="radio" name="search_stipend" value = 'yes' onClick="show_result()">yes</label>
+										<label class="radio-inline"><input type="radio" name="search_stipend" value = 'no' onClick="show_result()">no</label>
+									</div>
+								</div>
+								
+								<div class="form-group">
+									<label class="control-label">GPA: </label>
+									<div class="form-group row">
+										<div class="col-lg-6">
+											<input type="text" class="form-control" placeholder="min gpa" id="min_gpa" onKeyUp="show_result()"/>
+										</div>
+									 	<div class="col-lg-6">
+									 		<input type="text" class="form-control" placeholder="max gpa" id="max_gpa" onKeyUp="show_result()"/>
+									 	</div>
+										
+									</div>
+								</div>
+								
+							</form>
+			      			<div class="col-lg-2"></div>
+			      		</div>
+			    	</div>
+			  	</div>	  	
+			</div>
+		</div>
